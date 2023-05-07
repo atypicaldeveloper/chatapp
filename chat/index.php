7689a48284a8b7b24dd2a852a58048c9b0b48b32
atypicaldev <123357360+atypicaldeveloper@users.noreply.github.com>
2023-05-07 16:45:48 -0600

    Add files via upload

---
+++ chat/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Untitled Chat App</title> <!--title>Speak Out!</-->
+</head>
+
+<body>
+    <table id="maintable">
+        <tr>
+            <td id="channellist">
+                <h1 class="h1title">ChatApp</h1>
+                <h2 class="h2title">Channels</h2>
+            </td>
+            <td id="chat">
+                messages
+            </td>
+            <td id="rightsb">
+                <div id="channeldata">
+                    <h1 class="h2title">Channel Name</h1>
+                    <table id="info">
+                        <tr>
+                            <td>Site:</td>
+                            <td>*link*</td>
+                        </tr>
+                        <tr>
+                            <td>Group:</td>
+                            <td>*link*</td>
+                        </tr>
+                        <tr>
+                            <td>:3</td>
+                            <td>*link*</td>
+                        </tr>
+                    </table>
+                </div>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
